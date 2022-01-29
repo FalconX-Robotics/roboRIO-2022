08a6299ac250e70774640fb0e9962645846b681e
TaylorHart02 <kevlar2022@gmail.com>
2022-01-29 11:51:22 -0800

    Assigned Outtake command to button

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,9 +7,12 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.ArcadeDrive;
+import frc.robot.commands.OuttakeCommand;
 import frc.robot.commands.TankDrive;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Outtake;
 
 /**
 * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -32,13 +35,20 @@ public class RobotContainer {
 		configureButtonBindings();
 	}
 	
+	// Defines outtake subsystem
+	private final Outtake m_outtake = new Outtake();
+
 	/**
 	* Use this method to define your button->command mappings. Buttons can be created by
 	* instantiating a {@link GenericHID} or one of its subclasses ({@link
 	* edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
 	* edu.wpi.first.wpilibj2.command.button.JoystickButton}.
 	*/
-	private void configureButtonBindings() {}
+	
+	private void configureButtonBindings() {
+		new JoystickButton(m_driver, XboxController.Button.kA.value)
+		.toggleWhenPressed(new OuttakeCommand(m_outtake));
+	}
 	
 	/**
 	* Use this to pass the autonomous command to the main {@link Robot} class.
