48b1d7078540530aadf6a5a39e265d74641e21e8
RayBipse <raybipse@gmail.com>
2022-02-05 11:27:32 -0800

    Fixed some bugs on everything and we changed variable names

--- src/main/java/frc/robot/commands/TankDrive.java
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -36,6 +36,7 @@ public class TankDrive extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    m_drivetrain.tankDrive(0, 0);
   }
 
   // Returns true when the command should end.
