deab5d37a6355b0dfbba68660c44f9215043746b
taryn <tarynlanelew@gmail.com>
2022-10-06 16:18:54 -0700

    Temporary intake code

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -7,8 +7,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
-    private final CANSparkMax m_intake_motor_one = new CANSparkMax(Constants.INTAKE_MOTOR_ONE, MotorType.kBrushless);
-    private final CANSparkMax m_intake_motor_two = new CANSparkMax(Constants.INTAKE_MOTOR_TWO, MotorType.kBrushless);
+    private final CANSparkMax m_intake_wheel_motor = new CANSparkMax(Constants.INTAKE_WHEEL_MOTOR, MotorType.kBrushless);
+    private final CANSparkMax m_intake_arm_motor = new CANSparkMax(Constants.INTAKE_ARM_MOTOR, MotorType.kBrushless);
 
     public Intake() {}
 
@@ -16,9 +16,23 @@ public class Intake extends SubsystemBase {
         
     }
 
-    public void disableIntake() {
+    public void runIntake(double speed) {
         
     }
 
-    
+    public void disableIntake() {
+        runIntake(0);
+    }
+
+    public void raiseArm(double speed){
+        m_intake_arm_motor.set(speed);
+    }
+
+    public void lowerArm(double speed){
+        m_intake_wheel_motor.set(speed);
+    }
+
+    public double armAngle() {
+
+    }
 }
