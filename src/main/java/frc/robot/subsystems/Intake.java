9ed20e3fbff73a88a15d81e3d873d2d7fd96ba67
FatCactis <githublogan@gmail.com>
2022-10-06 17:08:53 -0700

    Added intake subsystem

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -9,15 +9,17 @@ import frc.robot.Constants;
 public class Intake extends SubsystemBase {
     private final CANSparkMax m_intake_wheel_motor = new CANSparkMax(Constants.INTAKE_WHEEL_MOTOR, MotorType.kBrushless);
     private final CANSparkMax m_intake_arm_motor = new CANSparkMax(Constants.INTAKE_ARM_MOTOR, MotorType.kBrushless);
+    private final double armGearRatio = (9.0/40.0) * (20.0 / 60.0) * (16.0 / 32.0);
+    //9:40  20:60  16:32
 
     public Intake() {}
 
     public void runIntake() {
-        
+        runIntake(0.0);
     }
 
     public void runIntake(double speed) {
-        
+        m_intake_wheel_motor.set(speed);
     }
 
     public void disableIntake() {
@@ -29,10 +31,14 @@ public class Intake extends SubsystemBase {
     }
 
     public void lowerArm(double speed){
-        m_intake_wheel_motor.set(speed);
+        m_intake_arm_motor.set(speed*-1);
     }
 
-    public double armAngle() {
-        return 0.;
+    public double getArmAngle() {
+        return m_intake_arm_motor.getEncoder().getPosition() * 360. * armGearRatio;
+    }
+
+    public void resetArmAngle() {
+        m_intake_arm_motor.getEncoder().setPosition(0);
     }
 }
